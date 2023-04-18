325ebfc77963345de0daca1ec7050723a1984af9
Ohad Segal <segalohad@gmail.com>
2023-04-19 01:56:45 +0300

    1st run

--- OhadSegal.java
+++ OhadSegal.java
@@ -2,7 +2,7 @@ public class OhadSegal {
     public static void main(String[] args) 
 	{
         for(int i = 1; i <= 3; i++) {
-            System.out.println("Ohad Segal, may the force be with you ! " + i);
+            System.out.println("Ohad Segal, may the force be with you " + i);
         }
     }
 }
