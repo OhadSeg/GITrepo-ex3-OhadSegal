fbfa7cee50f6b30717ca286a90f5d121853cc055
Ohad Segal <segalohad@gmail.com>
2023-04-19 02:10:45 +0300

    5/5

--- OhadSegal.java
+++ OhadSegal.java
@@ -2,7 +2,7 @@ public class OhadSegal {
     public static void main(String[] args) 
 	{
         for(int i = 1; i <= 3; i++) {
-            System.out.println("Ohad Segal, Hasta La vista Baby" + i);
+            System.out.println("Ohad Segal, live long and prosper " + i);
         }
     }
 }
